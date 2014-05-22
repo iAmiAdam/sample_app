be60fcd66baf62bb17510bf8ca30f1436faaa0af
Adam Smith <adam.smith18@ntlworld.com>
2014-05-22 12:06:01 +0100

    Added Sign in Helper, settings links

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ SampleApp::Application.routes.draw do
   match '/', to: 'static_pages#home', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
   match '/signin', to: 'sessions#new', via: 'get'
+  
   match '/signout', to: 'sessions#destroy', via: 'delete'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -76,7 +76,6 @@ describe "UserPages" do
 
     describe "with invalid information" do
       before { click_button "Save changes" }
-
       it { should have_content('error') }
     end
   end
