5e8e071b07a2f5e6aa96fe335e69993ee8656bee
Adam Smith <adam.smith18@ntlworld.com>
2014-05-23 10:19:22 +0100

    Finished basic microposts functionality

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -51,7 +51,7 @@ describe "UserPages" do
   let(:user) { FactoryGirl.create(:user) }
 
   describe "profile page" do
-    #let(:user) { FactoryGirl.create(:user) }
+    let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
